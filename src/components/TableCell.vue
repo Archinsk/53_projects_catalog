28b8114a9f176ac9070774ba4bb3fbf53bf4ea87
Михаил Васильев <vasiliev@2b.group>
2022-04-14 17:51:13 +0700

    projects table from DB

---
+++ src/components/TableCell.vue
@@ -0,0 +1,12 @@
+<template>
+  <td>{{ cell }}</td>
+</template>
+
+<script>
+export default {
+  name: "TableCell",
+  props: ["cell"],
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/TableRow.vue
@@ -0,0 +1,43 @@
+<template>
+  <tr>
+    <!--    <th scope="row">{{ row.id }}</th>-->
+    <!--    <td>{{ row.num }}</td>-->
+    <!--    <td>{{ row.name }}</td>-->
+    <!--    <td>Сайт для тестирования и демострации личных проектов</td>-->
+    <!--    <td></td>-->
+    <!--    <td>-->
+    <!--      <a href="https://www.d-skills.ru/index.html" target="_blank"-->
+    <!--        >https://www.d-skills.ru/index.html</a-->
+    <!--      >-->
+    <!--    </td>-->
+    <!--    <td>HTML, Bootstrap 4.4.1</td>-->
+    <!--    <td>-&#45;&#45;</td>-->
+    <!--    <td>-&#45;&#45;</td>-->
+    <!--    <td>Демонстрационный</td>-->
+    <!--    <td>Надо доделать/переделать</td>-->
+    <!--    <td>80%</td>-->
+    <!--    <td>Хостинг</td>-->
+    <!--    <td>-</td>-->
+    <!--    <td>+</td>-->
+    <!--    <td>-->
+    <!--      «-» - неполный перечень проектов, грубый дизайн, крупные карточки-->
+    <!--      мобильной версии-->
+    <!--    </td>-->
+
+    <TableCell v-for="(cellItem, index) of row" :key="index" :cell="cellItem" />
+  </tr>
+</template>
+
+<script>
+import TableCell from "./TableCell";
+
+export default {
+  name: "TableRow",
+  components: {
+    TableCell,
+  },
+  props: ["row"],
+};
+</script>
+
+<style scoped></style>
